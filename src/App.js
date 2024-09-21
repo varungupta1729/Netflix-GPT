9ceab2e2cef287adeeaf3048859b99487cb1353c
Varun Gupta <varungupta1228@gmail.com>
2024-09-21 16:02:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='bg-slate-400'>Hello Varun</div>
+    </div>
+  );
+}
+
+export default App;
