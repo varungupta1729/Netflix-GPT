ea07baa090fbec23852b025aff67b0458809ba45
Varun Gupta <varungupta1228@gmail.com>
2024-12-06 16:07:26 +0530

    made it responsive

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -55,43 +55,14 @@ const Header = () => {
       });
   };
   return (
-    <div className="absolute w-full z-10 flex justify-center md:justify-between px-12 py-3 bg-gradient-to-b from-black ">
+    <>
+    <div className="absolute w-full z-10  justify-center md:justify-between px-12 py-3 bg-gradient-to-b from-black hidden md:flex ">
       <div className={`flex justify-center items-center  md:flex-row ${toggle ? "flex-col" : "flex-row"} `}>
         {/* <img className="w-40 hidden md:flex " alt="logo" src={LOGO} /> */}
         <h1 className="text-[#ffbf00] text-4xl">MovieGPT</h1>
         <div className="flex  w-screen px-6 justify-between items-center md:hidden ">
-          <img className="w-32 " alt="logo" src={LOGO} />
-          {!toggle ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="30"
-              height="30"
-              viewBox="0 0 50 50"
-              onClick={() => setToggle(!toggle)}
-            >
-              <path
-                d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"
-                fill="white"
-              ></path>
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="30"
-              height="30"
-              viewBox="0 0 50 50"
-              onClick={() => setToggle(!toggle)}
-            >
-              <path
-                d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"
-                fill="white"
-              ></path>
-            </svg>
-          )}
+          {/* <img className="w-32 " alt="logo" src={LOGO} /> */}
+       
         </div>
 
         {user && (
@@ -143,6 +114,82 @@ const Header = () => {
         </div>
       )}
     </div>
+    <div className="absolute w-full z-10  justify-center  px-7 py-5 bg-gradient-to-b from-black md:hidden  ">
+      <div className="flex w-full justify-between items-center">
+      <h1 className="text-[#ffbf00] text-4xl">MovieGPT</h1>
+      {user&&(!toggle  ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="30"
+              height="30"
+              viewBox="0 0 50 50"
+              onClick={() => setToggle(!toggle)}
+            >
+              <path
+                d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"
+                fill="white"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="30"
+              height="30"
+              viewBox="0 0 50 50"
+              onClick={() => setToggle(!toggle)}
+            >
+              <path
+                d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"
+                fill="white"
+              ></path>
+            </svg>
+          ))}
+      </div>
+
+      <div>
+        {
+          toggle && (    <ul className={`w-full text-white  gap-4  md:flex   text-sm ${toggle ? "flex flex-col items-center justify-center" : "hidden"}`}>
+            <li
+              className="cursor-pointer text-[0.7rem] md:text-base"
+              onClick={() => navigate("/nowplaying")}
+            >
+              Now Playing
+            </li>
+            <li
+              className="cursor-pointer  text-[0.7rem] md:text-base"
+              onClick={() => navigate("/popular")}
+            >
+              Popular
+            </li>
+            <li
+              className="cursor-pointer  text-[0.7rem] md:text-base"
+              onClick={() => navigate("/toprated")}
+            >
+              Top Rated
+            </li>
+            <li
+              className="cursor-pointer  text-[0.7rem] md:text-base"
+              onClick={() => navigate("/upcoming")}
+            >
+              Upcoming
+            </li>
+            <li>
+            {
+              toggle && <button className="bg-[#ffbf00] text-white py-1 px-2  rounded md:hidden" onClick={handleSignout}>Logout</button>
+            }
+            </li>
+           
+          </ul>)
+        }
+
+      </div>
+
+    </div>
+    </>
   );
 };
 
--- src/component/VideoTitle.jsx
+++ src/component/VideoTitle.jsx
@@ -7,7 +7,7 @@ const VideoTitle = ({ title, overview , movieId}) => {
     <div className="w-full aspect-video absolute px-10 text-white bg-gradient-to-r from-black">
       <div className="w-2/3 lg:w-1/3 h-full flex flex-col justify-center gap-2 lg:gap-7 mt-16 lg:mt-0">
         <h1 className="text-2xl  lg:text-5xl ">{title}</h1>
-        <p className="text-[0.5rem]  lg:text-base">{overview}</p>
+        <p className="text-[0.5rem]  lg:text-base">{overview.length > 200 ? overview.slice(0 , 200) + "..." : overview }</p>
         <div className="flex text-[0.5rem] md:text-base  gap-4">
           <button onClick={()=> navigate('/player/'+movieId)} className="rounded  bg-[#ffbf00] text-black px-3 py-1  lg:px-10  lg:py-2">
              Play
